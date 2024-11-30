e3e62474ec86fd41a40f7fa71d895b79209b69b5
phantsam <rakjoshi123@gmail.com>
2024-12-01 02:23:02 +0530

    App

--- app/src/androidTest/java/com/mag/taskmanager/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mag/taskmanager/ExampleInstrumentedTest.java
@@ -3,7 +3,9 @@ package com.mag.taskmanager;
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnitRunner;
+
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,7 +18,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class ExampleInstrumentedTest { 
     @Test
     public void useAppContext() {
         // Context of the app under test.
